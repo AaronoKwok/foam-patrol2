54c642f3ca472fcd50dec988fd5088df472dfa71
Aaronokwok <aaronkwok101@gmail.com>
2022-07-20 12:14:31 -0700

    working on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,21 @@
 import React from "react" 
+import {Link} from "react-router-dom"
 
 function Header() {
     return (
         <header>
-            <h1>hello</h1>
+            <h1>Logo</h1>
+            <section>
+                <p>Surf Trips</p>
+                <p>About</p>
+                <p>Book Now</p>
+                <p>Contact Us</p>
+            </section>
+            <p>
+                contact buttons
+            </p>
+
+            
         </header>
     )
 }
