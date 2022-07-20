3614435b6c0c09a552f267408a07b675b688bcea
Aaronokwok <aaronkwok101@gmail.com>
2022-07-20 09:26:59 -0700

    more setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Foam-Patrol surfsite clone...
+# Foam-Patrol 
+
+## Ever wanted to learn how to surf? Well this is the site for you!
+
+### Tech used: React, JavaScript
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react" 
+import ReactDOM from "react-dom"
+
+import App from "./App"
+//broswer router , need first in dependencies
+
+ReactDOM.render(<App />, document.getElementById("root"))
