92a5779572e7c769ccf878ad9b34ac85a0a872d3
Aaronokwok <aaronkwok101@gmail.com>
2022-07-22 13:05:55 -0700

    header additions

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react" 
+
+function Logo() {
+    return (
+        <div>
+            <p>FOAM</p>
+            <p>PATROL.</p>
+            <p className="travel">TRAVEL</p>
+        </div>
+    )
+}
+
+export default Logo
--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,20 @@
 import React, {useHistory} from "react" 
 import {Link} from "react-router-dom"
 //import {Context} from "../Context"
+import Logo from "./Logo"
 
 import githubSVG from "../images/github.svg"
 import linkedinSVG from "../images/linkedin.svg"
 
 function Header() {
     //const {changeAesthetic} = useContext(Context)
+    //maybe put border color change in state
 
     return (
         <header className="navbar">
-            <h1>Logo</h1>
+
+            <Logo />
+
             <section className="whiteInNav">
                 <p className="navbuttons">SURF TRIPS &nbsp;<i className="arrow down"></i></p>
                 <p className="navbuttons">ABOUT &nbsp;<i className="arrow down"></i></p>
@@ -18,12 +22,16 @@ function Header() {
                 <p className="navbuttons "><Link to="/contactUs">CONTACT US</Link></p>
                 <p className="navbuttons"><Link to="/onlineCourses">ONLINE COURSES</Link></p>
             </section>
+
                 {/* <button onClick={changeAesthetic}></button>  */}
+                
             <div className="socials">
                 <a className="gh" target="_blank" href="https://github.com/AaronoKwok" rel="noreferrer"><img src={githubSVG} alt=""></img></a>
                 <a className="lkn" target="_blank" href="https://www.linkedin.com/in/aaronkwok1/" rel="noreferrer"><img src={linkedinSVG} alt=""></img></a>
             </div> 
+
             {/* <button>Login</button> */}
+            
         </header>
     )
 }
