4f29c9be248db2b2d78357af0e8beb594f64fe39
Aaronokwok <aaronkwok101@gmail.com>
2022-09-22 11:19:46 -0700

    intermediate retreat layout

--- src/pages/IntRetreat.js
+++ src/pages/IntRetreat.js
@@ -16,6 +16,82 @@ function IntRetreat({retreat}) {
                     </div>
                 </div>
             </div>
+            <section>
+                <p>Where:</p>
+                <p>{retreat.where.title}</p>
+                <p>{retreat.where.subtitle}</p>
+                <p>{retreat.where.des}</p>
+                <div className="main-info-bullets">
+                    {retreat.where.points.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+            </section>
+            <section>
+                <p>{retreat.typical.des}</p>
+            </section>
+            <section>
+                <p>Intermediate to Advanced Spots</p>
+                <p>{retreat.waves.des}</p>
+                <div className="main-info-bullets">
+                    {retreat.waves.spots.map((spot, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {spot}</p>
+                    })}
+                </div>
+                <p>{retreat.waves.note}</p>
+                <p>Intermediate Spots</p>
+                <p>{retreat.waves.intNote}</p>
+                <div className="main-info-bullets">
+                    {retreat.waves.spots.map((spot, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {spot}</p>
+                    })}
+                </div>
+            </section>
+            <section>
+                <p>{retreat.stay.method}</p>
+                <p>{retreat.stay.des}</p>
+                <div className="main-info-bullets">
+                    {retreat.stay.points.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+                <p>Rooms:</p>
+                <p>{retreat.stay.rooms.des}</p>
+                <div className="main-info-bullets">
+                    {retreat.stay.rooms.points.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+                <p>Meals:</p>
+                <p>{retreat.stay.meals.des}</p>
+                <div className="main-info-bullets">
+                    {retreat.stay.meals.points.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+                <p>{retreat.stay.meals.note}</p>
+            </section>
+            <p>Included:</p>
+            <section>
+                <p>Surf</p>
+                <div className="main-info-bullets">
+                    {retreat.stay.included.surf.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+                <p>Accommodation</p>
+                <div className="main-info-bullets">
+                    {retreat.stay.included.accommodation.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+                <p>Lifestyle</p>
+                <div className="main-info-bullets">
+                    {retreat.stay.included.lifestyle.map((point, index) => {
+                        return  <p><FontAwesomeIcon className="arrow-icon" icon={faAnglesRight} key={index}/> {point}</p>
+                    })}
+                </div>
+            </section>
         </div>
     )
 }
