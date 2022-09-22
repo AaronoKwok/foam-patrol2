f50942a9a80fd4cbac9779b28c1a2825f73ecc02
Aaronokwok <aaronkwok101@gmail.com>
2022-09-21 23:25:30 -0700

    retreat groundwork

--- src/pages/IntRetreat.js
+++ src/pages/IntRetreat.js
@@ -1,10 +1,21 @@
 import React from "react"
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAnglesRight} from '@fortawesome/free-solid-svg-icons'
+
 function IntRetreat({retreat}) {
     return (
         <div className="retreat-page">
-            <div className="filler"></div>
-            <p className="retreat-page-test">{retreat.location}</p>
+            <div className="retreat-bg-img-position">
+                <img className="retreat-backscene" src={retreat.imgs.background} alt=""/>
+                <div className="retreat-backscene-text">
+                    <div className="retreat-backscene-title">
+                        <p className="retreat-title">{retreat.location}</p>
+                        <p className="retreat-backscene-subtitle">{retreat.where.imgTitle}</p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
