fa96cf73fb199cb849cbdef2a9da92b71b1451da
Aaronokwok <aaronkwok101@gmail.com>
2022-09-27 20:01:12 -0700

    about

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,10 +1,78 @@
 import React from "react"
 
+import team from "../images/Team.jpeg"
+import elmo from "../images/elmo.jpeg"
+import bert from "../images/bert.jpeg"
+import bigBird from "../images/bigBird.jpeg" 
+import cookieMon from "../images/cookieMonster.jpeg"
+import countVon from "../images/countVon.jpeg"  
+import dorothy from "../images/dorothy.jpeg" 
+import ernie from "../images/ernie.jpeg" 
+import oscar from "../images/oscar.jpeg" 
+
 function Team() {
     return (
-        <div>
+        <div className="team-width">
             <div className="filler"></div>
-            Team
+
+            <div className="team-bar">
+                <div className="team-bar-description">
+                    <p className="team-title">Team</p>
+                    <p className="team-subtitle">Meet the Family</p>
+                    <p>We believe only dedicated, passionate human beings can create amazing surf travel experiences. This is why our focus is on building a family of highly professional team members from across the world. Each member of our team is carefully hand-picked, trained and committed to offer the best surf travel experiences for beginners & intermediates.</p>
+                </div>
+                <img className="team-img" src={team} alt=""/>
+            </div>
+
+            <section className="team-members">
+                <div className="team-person">
+                    <img className="team-portrait" src={elmo} alt=""/>
+                    <p className="person-name">Elmo</p>
+                    <p className="person-role">Co-Founder</p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={cookieMon} alt=""/>
+                    <p className="person-name">Cookie Monster</p>
+                    <p className="person-role">Co-Founder</p>
+                    <p className="person-des"></p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={bigBird} alt=""/>
+                    <p className="person-name">Big Bird</p>
+                    <p className="person-role">General Manager</p>
+                    <p className="person-des"></p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={ernie} alt=""/>
+                    <p className="person-name">Ernie</p>
+                    <p className="person-role">Head Surf Coach</p>
+                    <p className="person-des"></p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={oscar} alt=""/>
+                    <p className="person-name">Oscar</p>
+                    <p className="person-role">Head Surf Coach</p>
+                    <p className="person-des"></p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={countVon} alt=""/>
+                    <p className="person-name">Count Von Count</p>
+                    <p className="person-role">Surf Travel Director</p>
+                    <p className="person-des"></p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={dorothy} alt=""/>
+                    <p className="person-name">Dorothy</p>
+                    <p className="person-role">Surf Coach - Costa Rica</p>
+                    <p className="person-des"></p>
+                </div>
+                <div className="team-person">
+                    <img className="team-portrait" src={bert} alt=""/>
+                    <p className="person-name">Bert</p>
+                    <p className="person-role">Surf Coach - Bali</p>
+                    <p className="person-des"></p>
+                </div>
+            </section>            
         </div>
     )
 }
--- src/pages/WhatDo.js
+++ src/pages/WhatDo.js
@@ -1,10 +1,55 @@
 import React from "react" 
 
+import background from "../images/whatWeDoBack.jpeg"
+import why from "../images/why.jpeg"
+import whowe from "../images/whoWe.jpeg"
+
 function WhatDo() {
     return (
         <div>
             <div className="filler"></div>
-            What we do
+            <div className="whatDo-background">
+                <img className="whatDoBack" src={background} alt=""/>
+                <p className="whatDo-title">What We Do</p>
+                <p className="whatDo-sub">Our Mission</p>
+            </div>
+            <section className="team-width">
+                <div className="whatDo-holder">
+                    <div>
+                        <p>Why do we do what we do?</p>
+                        <p>We share our love for surfing because it changed our lives.</p>
+                        <p>From our experience, surfing becomes more fun the more you progress. We know there is a path to accelerate the process of learning to surf. We want to share it with you because we believe nothing is more satisfying, fun and rewarding than the feeling of learning to surf better on some of the best waves on earth.</p>
+                    </div>
+                    <img className="right-img" src={why} alt=""/>
+                </div>
+
+                <div className="whatDo-holder">
+                    <img className="left-img" src={whowe} alt="" />
+                    <div>
+                        <p>Who we are</p>
+                        <p>We are a family of passionate surf coaches dedicated to helping you learn to surf faster than you could ever imagine.</p>
+                        <p>Our intention has never been to “start a surf school”. Our goal was to create the ultimate surf coaching retreats for beginner and intermediate surfers. We do this by bringing you to some of the most consistent and fun waves in the world while sharing our knowledge in the clearest, most efficient way possible.</p>
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+            </section>
         </div>
     )
 }
