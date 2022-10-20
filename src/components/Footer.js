326b5c1f376e5ad79f916252c403cfdb0b7ec285
Aaronokwok <aaronkwok101@gmail.com>
2022-10-19 20:21:48 -0700

    desktop version, api calls ON

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,37 +14,61 @@ function Footer() {
             {
                 (useWindowWidth() === true) && 
                 <div>
-
+                    <section className="one-percent">
+                        <p className="one-percent-desc">1% of every sale goes to nonprofit organizations that work to clean and protect our oceans.</p>
+                        <a target="_blank" rel="noreferrer" href="https://onepercentfortheplanet.org/"><img className="one-percent-img" src={onePercent} alt="" /></a>
+                    </section>
+                    <section className="footer-mid">
+                        <div className="footer-internal-links">
+                            <p className="footer-surf">Surfing</p>
+                            <Link className="footer-surf-questions" to="/about/surfing-level"><p>What's my surfing level?</p></Link> 
+                            <Link className="footer-surf-questions" to="/about/is-this-trip-for-me"><p className="footer-second">Is this surf trip for me?</p></Link>
+                        </div>
+                        {/* <div>
+                            <p>Trip Advisor</p>
+                        </div> */}
+                        <div>
+                            <p className="footer-socials">Socials</p>
+                            <div className="footer-socials-background">
+                                <a className="gh" target="_blank" href="https://github.com/AaronoKwok" rel="noreferrer"><img src={githubSVG} alt=""></img></a>
+                                <a className="lkn" target="_blank" href="https://www.linkedin.com/in/aaronkwok1/" rel="noreferrer"><img src={linkedinSVG} alt=""></img></a>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="footer-base">
+                        <p className="footer-base-left">United States | English (US) | $ (USD)</p>
+                        <p className="footer-base-right">&#169; 2022 Foam Patrol. Travel | Images and design inspiration from <a className="barefoot" href="https://barefootsurftravel.com/" target="_blank" rel="noreferrer">Barefoot Surf</a></p>
+                    </section>
                 </div>
             }
             {
                 (useWindowWidth() === false) && 
                 <div>
                     <section className="one-percent">
-                    <p className="one-percent-desc">1% of every sale goes to nonprofit organizations that work to clean and protect our oceans.</p>
-                    <a target="_blank" rel="noreferrer" href="https://onepercentfortheplanet.org/"><img className="one-percent-img" src={onePercent} alt="" /></a>
-                </section>
-                <section className="footer-mid">
-                    <div className="footer-internal-links">
-                        <p className="footer-surf">Surfing</p>
-                        <Link className="footer-surf-questions" to="/about/surfing-level"><p>What's my surfing level?</p></Link> 
-                        <Link className="footer-surf-questions" to="/about/is-this-trip-for-me"><p className="footer-second">Is this surf trip for me?</p></Link>
-                    </div>
-                    {/* <div>
-                        <p>Trip Advisor</p>
-                    </div> */}
-                    <div>
-                        <p className="footer-socials">Socials</p>
-                        <div className="footer-socials-background">
-                            <a className="gh" target="_blank" href="https://github.com/AaronoKwok" rel="noreferrer"><img src={githubSVG} alt=""></img></a>
-                            <a className="lkn" target="_blank" href="https://www.linkedin.com/in/aaronkwok1/" rel="noreferrer"><img src={linkedinSVG} alt=""></img></a>
+                        <p className="one-percent-desc">1% of every sale goes to nonprofit organizations that work to clean and protect our oceans.</p>
+                        <a target="_blank" rel="noreferrer" href="https://onepercentfortheplanet.org/"><img className="one-percent-img" src={onePercent} alt="" /></a>
+                    </section>
+                    <section className="footer-mid">
+                        <div className="footer-internal-links">
+                            <p className="footer-surf">Surfing</p>
+                            <Link className="footer-surf-questions" to="/about/surfing-level"><p>What's my surfing level?</p></Link> 
+                            <Link className="footer-surf-questions" to="/about/is-this-trip-for-me"><p className="footer-second">Is this surf trip for me?</p></Link>
                         </div>
-                    </div>
-                </section>
-                <section className="footer-base">
-                    <p className="footer-base-left">United States | English (US) | $ (USD)</p>
-                    <p className="footer-base-right">&#169; 2022 Foam Patrol. Travel | Images and design inspiration from <a className="barefoot" href="https://barefootsurftravel.com/" target="_blank" rel="noreferrer">Barefoot Surf</a></p>
-                </section>
+                        {/* <div>
+                            <p>Trip Advisor</p>
+                        </div> */}
+                        <div>
+                            <p className="footer-socials">Socials</p>
+                            <div className="footer-socials-background">
+                                <a className="gh" target="_blank" href="https://github.com/AaronoKwok" rel="noreferrer"><img src={githubSVG} alt=""></img></a>
+                                <a className="lkn" target="_blank" href="https://www.linkedin.com/in/aaronkwok1/" rel="noreferrer"><img src={linkedinSVG} alt=""></img></a>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="footer-base">
+                        <p className="footer-base-left">United States | English (US) | $ (USD)</p>
+                        <p className="footer-base-right">&#169; 2022 Foam Patrol. Travel | Images and design inspiration from <a className="barefoot" href="https://barefootsurftravel.com/" target="_blank" rel="noreferrer">Barefoot Surf</a></p>
+                    </section>
                 </div>
             }
             
