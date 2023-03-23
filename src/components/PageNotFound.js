da75e7d88282cc88909faedc57f17a8f59f357fd
Aaron Kwok <aaronkwok101@gmail.com>
2023-03-23 12:49:29 -0700

    add exist page

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,9 @@
+function PageNotFound() {
+  	return (
+    	<div className="page-not-found">
+      		<p className="page-not-found-text">Error 404: Page Does Not Exist</p>
+    	</div>
+  	);
+}
+
+export default PageNotFound
